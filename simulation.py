66e11a25626d1b5c728703994ec219058db07a25
gleb skryabin <glebka.sk@gmail.com>
2019-01-17 23:22:16 +0300

    def quit

--- simulation.py
+++ simulation.py
@@ -5,16 +5,16 @@ from time import sleep
 
 
 
-class SpaseObject:
+class SpaceObject:
     def __init__(self, x, y, mass, *args):
         self.x = x
         self.y = y
         self.mass = mass
 
         if len(args) == 0 :
-            self.color = color_rbg(randrange(256), randrange(256), randrange(256))
+            self.color = color_rgb(randrange(256), randrange(256), randrange(256))
 
-        if len(args) == 1 :
+        elif len(args) == 1 :
             self.color = args[0]
 
 
@@ -22,10 +22,11 @@ class SpaseObject:
 
 class Universe:
     def __init__(self, *args):
-        self.status = 0
+        self.status = 0 # 0 - expectations; 1 - work; 2 - quit
         self.infoWidth = 30
         self.infoHeight = 16
         self.interation = 0
+        self.myobjects = []
         self.bgcolor = 'black'
         if len(args) == 2 :
             self.createWindow(args[0], args[1])
@@ -68,19 +69,31 @@ class Universe:
 
     def showInfo(self):
         #self.clearInfoList()
-        #infoText = Text(Point(self.width - self.infoWidth / 2, self.infoHeight / 2), self.interation)
-        #infoText.setFill('white')
-        #infoText.draw(self.window)
         self.infoText.setText(self.interation)
+
+    def newObject(self, x, y, mass, *args):
+        if len(args) == 0 :
+            self.myobjects.append(SpaceObject(x, y, mass))
+        elif len(args) == 1 :
+            self.myobjects.append(SpaceObject(x, y, mass, args[0]))
+
+        
+
 
     def show(self):
         self.showInfo()
 
+    def quit():
+        self.status = 2
+        self.__del__()
+        
     def universeLoop(self):
         while True :
             self.interation += 1
             self.show()
             time.sleep(0.5)
+            if self.interation == 3:
+                self.quit()
 
     def preStart(self):
         self.clearWindow()
@@ -95,12 +108,15 @@ class Universe:
     def startSimulation(self):
         self.preStart()
         self.status = 1
+
+        self.newObject(300, 300, 20)
+
         self.universeLoop()
-'''
+
     def __del__(self):
-        if self.status == 1 :
+        if self.status == 2 :
             self.window.close()
-            '''
+            
 
 
 
