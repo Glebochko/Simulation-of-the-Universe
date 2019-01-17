5360590cb93b2eccf2ec5f5d4cf7d6a20bf85a43
gleb skryabin <glebka.sk@gmail.com>
2019-01-17 22:39:27 +0300

    show info and clear window

--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 from graphics import *
 from random import randint
 from random import randrange
+from time import sleep
 
 
 
@@ -23,6 +24,9 @@ class Universe:
     def __init__(self, *args):
         self.status = 0
         self.infoWidth = 30
+        self.infoHeight = 16
+        self.interation = 0
+        self.bgcolor = 'black'
         if len(args) == 2 :
             self.createWindow(args[0], args[1])
 
@@ -30,11 +34,55 @@ class Universe:
     def createWindow(self, width, height):
         self.width = width
         self.height = height
-        self.window = GraphWin('Simulation of the Universe!', width + 1 + self.infoWidth, height)
+        self.window = GraphWin('Simulation of the Universe!', width, height)
+
+    def clearWindow(self):
+        p1 = Point(0, 0)
+        p2 = Point(self.width, 0)
+        p3 = Point(self.width, self.height)
+        p4 = Point(0, self.height)
+
+        vertices = [p1, p2, p3, p4]
+
+        cleanWindow = Polygon(vertices)
+        cleanWindow.setOutline(self.bgcolor)
+        cleanWindow.setFill(self.bgcolor)
+
+        cleanWindow.draw(self.window)
+
+    def clearInfoList(self):
+        p1 = Point(self.width - self.infoWidth - 1, 0)
+        p2 = Point(self.width - 1, 0)
+        p3 = Point(self.width - 1, self.infoHeight)
+        p4 = Point(self.width - self.infoWidth - 1, self.infoHeight)
+
+        vertices = [p1, p2, p3, p4]
+
+        cleanInfoList = Polygon(vertices)
+        cleanInfoList.setOutline('green')
+        cleanInfoList.setFill(self.bgcolor)
+
+        cleanInfoList.draw(self.window)
+
+    def showInfo(self):
+        self.clearInfoList()
+        info = Text(Point(self.width - self.infoWidth / 2, self.infoHeight / 2), self.interation)
+        info.setFill('white')
+        info.draw(self.window)
+
+    def show(self):
+        self.showInfo()
+
+    def universeLoop(self):
+
+        while True :
+            self.interation += 1
+            self.show()
+            time.sleep(0.5)
 
     def startSimulation(self):
-        self.showInfo()
-        pass
+        self.status = 1
+        self.universeLoop()
 
     def __del__(self):
         if self.status == 1 :
